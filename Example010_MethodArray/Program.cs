7b7e45955dfe381aab00a7b77ea408607a34c2f5
Elisabeth <m.e.elisa@yandex.ru>
2023-02-18 14:08:27 +0300

    Сохранена задача 10

---
+++ Example010_MethodArray/Program.cs
@@ -0,0 +1,15 @@
+﻿int[] array = {11, 21, 35, 46, 85, 63, 77, 28, 45555};
+
+int n=array.Length;
+int find=77;
+
+int index=0;
+
+while(index<n)
+{
+    if(array[index]==find)
+    {
+        System.Console.WriteLine(index);
+    }
+    index++;
+}
